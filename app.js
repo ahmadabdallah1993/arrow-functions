6e8f61c86288659d0e96a790b64e579381a5956f
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-19 22:24:02 +0300

    done

--- app.js
+++ app.js
@@ -249,17 +249,24 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//it is showing the different values based on the situation or things that the function is called (the obj that called the function).
+
+
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// it is use the value of this in their lexical scope 
+//(it shows all the variables visible to a function where it is defined,
+//and it shows things is already called, and the other things who it didnt called and how it is defined).
+
+
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//because it shows all the variables visible to a function where it is defined,
+//and it shows things is already called, and the other things who it didnt called and how it is defined).
